f93930f9ceb11e7ffc9dea2870777214cf04d030
Ivan Volkov <volkov.ivan2004@gmail.com>
2023-03-05 03:20:10 +0300

    Add multithreading (Task A)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,34 @@
 import rawhttp.core.RawHttp
 import java.io.File
 import java.net.ServerSocket
+import java.net.Socket
+import kotlin.concurrent.thread
+
+fun handleClient(socket: Socket) = socket.use {
+    val request = RawHttp().parseRequest(it.getInputStream())
+
+    if (request.method != "GET") {
+        Responder.badRequestResponse.writeTo(it.getOutputStream())
+        return@use
+    }
+
+    val uri = "data" + request.uri.path
+    val file = File(uri)
+    if (!file.isFile) {
+        Responder.notFoundResponse.writeTo(it.getOutputStream())
+        return@use
+    }
+
+    Responder.getOkResponse(file).writeTo(it.getOutputStream())
+}
 
 fun main() {
-    val rawHttp = RawHttp()
     val server = ServerSocket(8080)
 
     while (true) {
-        server.accept().use { socket ->
-            val request = rawHttp.parseRequest(socket.getInputStream())
-
-            if (request.method != "GET") {
-                Responder.badRequestResponse.writeTo(socket.getOutputStream())
-                return@use
-            }
-
-            val uri = "data" + request.uri.path
-            val file = File(uri)
-            if (!file.isFile) {
-                Responder.notFoundResponse.writeTo(socket.getOutputStream())
-                return@use
-            }
-
-            Responder.getOkResponse(file).writeTo(socket.getOutputStream())
+        val socket = server.accept()
+        thread {
+            handleClient(socket)
         }
     }
 }
